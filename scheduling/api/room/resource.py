f52e78397ea023b0c27ab09a485b98e4c21aeb8f
fcschmidt <fcschimidt@gmail.com>
2019-02-02 11:40:16 -0200

    Renamed File

---
+++ scheduling/api/room/resource.py
@@ -0,0 +1,16 @@
+from flask_restful import Resource
+
+
+class RoomAPI(Resource):
+
+    def post(self, id):
+        pass
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+
+
