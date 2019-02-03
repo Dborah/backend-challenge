62e21c59c855c7b71089646a356bc8caef76153f
fcschmidt <fcschimidt@gmail.com>
2019-02-03 14:10:26 -0200

    Added Config ENV

--- scheduling/app_factory.py
+++ scheduling/app_factory.py
@@ -1,24 +1,29 @@
 from flask import Flask
 
 from scheduling.ext.db import db
+from scheduling.ext import migrate
+
+from settings import app_config
 
 
-def create_app():
+def create_app(env_name):
     app = Flask(__name__)
 
-    # Initialize Extensions
-    register_blueprint(app)
+    app.config.from_object(app_config[env_name])
 
-    # Initialize blueprints
-    register_blueprint(app)
+    # Initialize Extensions
+    register_extensions(app)
+
+    # Initialize Blueprints
+    register_blueprints(app)
 
     return app
 
 
-def register_blueprint(app):
+def register_blueprints(app):
     """Register Blueprints"""
-    from scheduling.api.room import bp as room_api
-    from scheduling.api.schedule import bp as schedule_api
+    from scheduling.blueprints.api.room import resource as room_api
+    from scheduling.blueprints.api.schedule import bp as schedule_api
 
     room_api.init_app(app)
     schedule_api.init_app(app)
@@ -28,4 +33,5 @@ def register_blueprint(app):
 def register_extensions(app):
     """Register extensions."""
     db.init_app(app)
+    migrate.init_app(app, db)
     return app
