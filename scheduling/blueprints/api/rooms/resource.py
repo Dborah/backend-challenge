a802901636de396d37eb0c372f912c0ff5742516
fcschmidt <fcschimidt@gmail.com>
2019-02-05 11:57:08 -0200

    Update Resource

--- scheduling/blueprints/api/rooms/resource.py
+++ scheduling/blueprints/api/rooms/resource.py
@@ -19,22 +19,45 @@ resource_fields = {
 }
 
 
-class RoomAPI(Resource):
+class Rooms(Resource):
 
     def __init__(self):
         self.args = room_parser.parse_args()
 
-    @staticmethod
-    def get(room_id):
-        query_room = Room.get_one_room(room_id)
-        error_404(query_room, room_id, 'room')
-        resp = {'id': query_room.id, 'room_number': query_room.room_number}
-        return jsonify(resp)
+    # @staticmethod
+    # def get():
+    #     query = Room.get_all_room()
+    #     resp = room_serializer(query)
+    #     if len(resp) == 0:
+    #         return [{'message': 'No registered rooms.'}][0]
+    #     resp = (resp, 200)
+    #     return resp
+
+    def post(self):
+        room_id = self.args['room_number']
+        room = Room(room_number=room_id)
+        room.save()
+        # Adicionar erro de conflito com sala já existente
+        resp = [{'message': 'New rooms created successfully.'}, {'rooms': room_id}]
+        return resp, 201
+
+
+class RoomItem(Resource):
+
+    def __init__(self):
+        self.args = room_parser.parse_args()
+
+    # @staticmethod
+    # def get(room_id):
+    #     query_room = Room.get_one_room(room_id)
+    #     error_404(query_room, room_id, 'rooms')
+    #     resp = {'id': query_room.id, 'room_number': query_room.room_number}
+    #     return jsonify(resp)
 
     def put(self, room_id):
         room_number = self.args['room_number']
         query_room = Room.get_one_room(room_id)
-        error_404(query_room, room_id, 'room')
+        error_404(query_room, room_id, 'rooms')
         # Adicionar erro de conflito com sala já existente
         room = Room.update(query_room, room_number)
         resp = {'message': 'Room updated successfully.'}, {'room_number': room.room_number}
@@ -43,36 +66,13 @@ class RoomAPI(Resource):
     @staticmethod
     def delete(room_id):
         query_room = Room.get_one_room(room_id)
-        error_404(query_room, room_id, 'room')
+        error_404(query_room, room_id, 'rooms')
         query_room.delete()
         # Adicionar resposta de retorno, tratar erros e conflitos
         return query_room
 
 
-class RoomListAPI(Resource):
-
-    def __init__(self):
-        self.args = room_parser.parse_args()
-
-    @staticmethod
-    def get():
-        query = Room.get_all_room()
-        resp = room_serializer(query)
-        if len(resp) == 0:
-            return [{'message': 'No registered room.'}][0]
-        resp = (resp, 200)
-        return resp
-
-    def post(self):
-        room_id = self.args['room_number']
-        room = Room(room_number=room_id)
-        room.save()
-        # Adicionar erro de conflito com sala já existente
-        resp = [{'message': 'New room created successfully.'}, {'room': room_id}]
-        return resp, 201
-
-
 def init_app(app):
-    api.add_resource(RoomListAPI, '/rooms', endpoint='rooms')
-    api.add_resource(RoomAPI, '/rooms/<int:room_id>', endpoint='room')
+    api.add_resource(Rooms, '/rooms', endpoint='rooms')
+    api.add_resource(RoomItem, '/rooms/<int:room_id>', endpoint='room')
     app.register_blueprint(bp_rest)
