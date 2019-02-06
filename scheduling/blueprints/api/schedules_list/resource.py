0fe846419dbd3730aa2ed62a9b3843d3ede31e4d
fcschmidt <fcschimidt@gmail.com>
2019-02-06 03:21:00 -0200

    Created Resource: Filter Scheduling

--- scheduling/blueprints/api/schedules_list/resource.py
+++ scheduling/blueprints/api/schedules_list/resource.py
@@ -5,6 +5,10 @@ from scheduling.blueprints.api.models.scheduling_model import Scheduling as Sche
 
 from scheduling.blueprints.api.utils import schedule_serializer
 
+from scheduling.blueprints.api.responses import (
+    resp_does_not_exist,
+    resp_successful
+    )
 
 bp_rest = Blueprint('schedule_api', __name__, url_prefix='/api/v1')
 api = Api(bp_rest)
@@ -12,7 +16,7 @@ api = Api(bp_rest)
 
 schedule_parser = reqparse.RequestParser()
 schedule_parser.add_argument('date', type=str)
-schedule_parser.add_argument('room_number', type=str)
+schedule_parser.add_argument('room', type=str)
 
 
 class SchedulesFilter(Resource):
@@ -24,9 +28,28 @@ class SchedulesFilter(Resource):
         date = self.args['date']
         room_number = self.args['room_number']
 
+        if not date and not room_number:
+            return None
+
+        if not date:
+            query_schedules = SchedulingModel.filter_room_number(room_number)
+            serialized = schedule_serializer(query_schedules)
+            return resp_successful(serialized)
+
+        if not room_number:
+            query_schedules = SchedulingModel.filter_date(date)
+            serialized = schedule_serializer(query_schedules)
+            return resp_successful(serialized)
+
+        if not SchedulingModel.filter_date(date):
+            return resp_does_not_exist(None, 'Date')
+
+        if not SchedulingModel.filter_room_number(room_number):
+            return resp_does_not_exist(None, 'Room')
+
         query_schedules = SchedulingModel.filter_schedules(date, room_number)
-        response = schedule_serializer(query_schedules)
-        return response
+        serialized = schedule_serializer(query_schedules)
+        return resp_successful(serialized)
 
 
 def init_app(app):
