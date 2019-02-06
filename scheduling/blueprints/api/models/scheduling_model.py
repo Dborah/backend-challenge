4b044c057a3968c69255922e260f7e2ba456fb8d
fcschmidt <fcschimidt@gmail.com>
2019-02-06 17:00:57 -0200

    Created all queries

--- scheduling/blueprints/api/models/scheduling_model.py
+++ scheduling/blueprints/api/models/scheduling_model.py
@@ -1,21 +1,15 @@
 from scheduling.ext.db import db
 
-# from flask_sqlalchemy import orm
-
 
 class Scheduling(db.Model):
     """Schedules model."""
-    __tablename__ = 'scheduling'
+    __tablename__ = 'schedules'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
-    start_time = db.Column(db.String(50), nullable=False)
-    end_time = db.Column(db.String(50), nullable=False)
-
+    room_number = db.Column(db.Integer, nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey('rooms.id'), nullable=False)
-    room = db.relationship('Room', backref=db.backref('scheduling', lazy='dynamic'))
-    room_number = db.Column(db.String(50), nullable=False)
 
     def save(self):
         db.session.add(self)
@@ -29,30 +23,30 @@ class Scheduling(db.Model):
     def update(scheduling, data):
         scheduling.title = data['title']
         scheduling.date = data['date']
-        scheduling.start_time = data['start_time']
-        scheduling.end_time = data['end_time']
         scheduling.room_number = data['room_number']
         scheduling.room_id = data['room_id']
         db.session.commit()
 
     @staticmethod
-    def get_all_schedule():
+    def get_schedules():
         return Scheduling.query.all()
 
     @staticmethod
-    def get_one_scheduling(schedule_id):
-        return Scheduling.query.get(schedule_id)
+    def get_scheduling(scheduling_id):
+        return Scheduling.query.get(scheduling_id)
 
     @staticmethod
     def filter_date(date):
         return Scheduling.query.filter(Scheduling.date == date).all()
 
     @staticmethod
-    def filter_room(room):
+    def filter_room_number(room):
         return Scheduling.query.filter(Scheduling.room_number == room).all()
 
-    def __repr__(self):
-        return f'schedule(id={self.id}, title={self.title}, date={self.date}, ' \
-            f'start_time={self.start_time}, end_time{self.end_time}, ' \
-            f'room_id={self.room_id}, room_number={self.room_number})!r'
+    @staticmethod
+    def filter_schedules(date, room):
+        return Scheduling.query.filter(
+            db.and_(Scheduling.date == date, Scheduling.room_number == room)).all()
 
+    def __repr__(self):
+        return f'schedule(id={self.id}, title={self.title}, date={self.date}, room_number={self.room_number})'
