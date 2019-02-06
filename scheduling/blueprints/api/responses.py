e1e97ed85600e329baa14bb95639645494dd13c7
fcschmidt <fcschimidt@gmail.com>
2019-02-06 16:33:42 -0200

    Created unavailable response

--- scheduling/blueprints/api/responses.py
+++ scheduling/blueprints/api/responses.py
@@ -6,63 +6,84 @@ def resp_data_invalid():
     pass
 
 
+def resp_already_exists(msg):
+    """Response 400"""
+    response = jsonify({
+        'message': f'{msg} already exists.'
+    })
+    response.status_code = 400
+    return response
+
+
+def resp_unavailable():
+    """Response 400"""
+    response = jsonify({
+        'message': f'Room Unavailable for this Period.'
+    })
+    response.status_code = 400
+    return response
+
+
+def resp_room_linked_to_schedule(msg):
+    """Response 401"""
+    response = jsonify({
+        'message': f'This room {msg} has been reserved for one or more meetings. '
+        f'Please choose another one or try updating it. Or removed to Schedule'
+    })
+    response.status_code = 401
+    return response
+
+
+def resp_not_allowed():
+    """Response 405 Not Allowed"""
+    response = jsonify({
+        "message": "The method is not allowed for the requested URL."
+    })
+    response.status_code = 405
+    return response
+
+
 def resp_does_not_exist(resource, msg):
     """Response 404 Not Found"""
     if not resource:
         abort(http_status_code=404, message=f"{msg} does not exist.")
 
 
-def resp_already_exists(msg):
-    """Response 400"""
-    resp = jsonify({
-        'message': f'{msg} already exists.'
-    })
-    resp.status_code = 400
-    return resp
-
-
-def resp_room_linked_to_schedule(msg):
-    """Response 401"""
-    resp = jsonify({
-        'message': f'This room {msg} has been reserved for one or more meetings. '
-        f'Please choose another one or try updating it. Or removed to Schedule'
-    })
-    resp.status_code = 401
-    return resp
+def resp_successful(resource):
+    """ Response 200"""
+    response = jsonify(resource)
+    response.status_code = 200
+    return response
 
 
 def resp_created_successfully(msg):
     """Response 201"""
-    resp = jsonify({
+    response = jsonify({
         'message': f'New {msg} created successfully.'
     })
-    resp.status_code = 201
-    return resp
+    response.status_code = 201
+    return response
 
 
 def resp_update_successfully(msg):
     """Response 201"""
-    resp = jsonify({
+    response = jsonify({
         'message': f'{msg} updated successfully.'
     })
-    resp.status_code = 201
-    return resp
+    response.status_code = 201
+    return response
 
 
 def resp_delete_successfully(msg):
     """Response 202"""
-    resp = jsonify({
+    response = jsonify({
         'message': f'{msg} delete successfully.'
     })
-    resp.status_code = 202
-    return resp
+    response.status_code = 202
+    return response
 
 
-def resp_successful(resource):
-    """ Response 200"""
-    resp = jsonify(resource)
-    resp.status_code = 200
-    return resp
+
 
 
 
