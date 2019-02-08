8a488491c1f415464178153fe83c412760ef0cad
fcschmidt <fcschimidt@gmail.com>
2019-02-08 18:33:30 -0200

    Updated Settings

--- settings.py
+++ settings.py
@@ -1,36 +1,32 @@
-# from decouple import config
-#
-# DEBUG = config('DEBUG', default=False, cast=bool)
-# DATABASE_LOCATION = config('DATABASE_LOCATION')
-# DATABASE_NAME = config('DATABASE_NAME')
-# SQLALCHEMY_DATABASE_URI = f'{DATABASE_LOCATION}/{DATABASE_NAME}'
-# SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', default=False, cast=bool)
-# DEBUG_TB_INTERCEPT_REDIRECTS = False
-# SECRET_KEY = config('SECRET_KEY')
 import os
 
 
-class Development(object):
+class Config:
+    DEBUG = os.getenv('DEBUG') or False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+class Development(Config):
     """Development environment configuration"""
     DEBUG = True
-    TESTING = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
-class Production(object):
+class Production(Config):
     """Production environment configurations"""
     DEBUG = False
     TESTING = False
+
+
+class Testing(Config):
+    """Testing environment configurations"""
+    DEBUG = True
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-
-
-class Testing(object):
-    pass
 
 
 app_config = {
     'development': Development,
     'production': Production,
+    'testing': Testing
 }
