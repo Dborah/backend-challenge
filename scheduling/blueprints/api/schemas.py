94edcfda48055c349b79986c3e101c9206f45263
fcschmidt <fcschimidt@gmail.com>
2019-02-08 17:32:03 -0200

    Update schemas

--- scheduling/blueprints/api/schemas.py
+++ scheduling/blueprints/api/schemas.py
@@ -8,7 +8,7 @@ class RoomSchema(Schema):
 
 class ScheduleSchema(Schema):
     id = fields.Integer(dump_only=True)
-    title = fields.Integer(dump_only=True)
+    title = fields.String(dump_only=True)
     date = fields.String(dump_only=True)
-    room_id = fields.Integer(dump_only=True)
     room_number = fields.String(dump_only=True)
+    room_id = fields.Integer(dump_only=True)
