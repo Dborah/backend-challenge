32e54c50ca203706bb6185ecb8d44f0cf5fc7f64
fcschmidt <fcschimidt@gmail.com>
2019-02-09 12:15:38 -0200

    Imported errors

--- scheduling/blueprints/api/rooms/resource.py
+++ scheduling/blueprints/api/rooms/resource.py
@@ -5,12 +5,15 @@ from scheduling.blueprints.api.models.room_model import Room as RoomModel
 from scheduling.blueprints.api.models.scheduling_model import Scheduling as SchedulingModel
 
 from scheduling.blueprints.api.responses import (
-    resp_does_not_exist,
-    resp_already_exists,
     resp_created_successfully,
     resp_update_successfully,
     resp_delete_successfully,
-    resp_room_linked_to_schedule
+    )
+
+from scheduling.blueprints.api.errors import (
+    error_does_not_exist,
+    error_already_exists,
+    error_room_linked_to_schedule
     )
 
 bp_rest = Blueprint('room_api', __name__, url_prefix='/api/v1')
@@ -40,7 +43,7 @@ class Rooms(Resource):
             return resp_created_successfully('room')
 
         if query_room.room_number == room_number:
-            return resp_already_exists('Room')
+            return error_already_exists('Room')
 
 
 class RoomItem(Resource):
@@ -54,21 +57,21 @@ class RoomItem(Resource):
 
         if not filter_room:
             query_room = RoomModel.get_room(room_id)
-            resp_does_not_exist(query_room, f'Room {room_id}')
+            error_does_not_exist(query_room, f'Room {room_id}')
             RoomModel.update(query_room, room_number)
             return resp_update_successfully('Room')
 
         if filter_room.room_number == room_number:
-            return resp_already_exists('Room')
+            return error_already_exists('Room')
 
     @staticmethod
     def delete(room_id):
         query_room = RoomModel.get_room(room_id)
-        resp_does_not_exist(query_room, f'Room {room_id}')
+        error_does_not_exist(query_room, f'Room {room_id}')
 
         room_filter = SchedulingModel.filter_room_id(room_id)
         if room_filter:
-            return resp_room_linked_to_schedule(f'{room_id}')
+            return error_room_linked_to_schedule(f'{room_id}')
         query_room.delete()
         return resp_delete_successfully('Room')
 
