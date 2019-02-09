0d6e79d543ddad2c762023151aca2380491e6e4e
fcschmidt <fcschimidt@gmail.com>
2019-02-09 12:11:55 -0200

    Removed Errors for errors.py

--- scheduling/blueprints/api/responses.py
+++ scheduling/blueprints/api/responses.py
@@ -1,52 +1,4 @@
 from flask import jsonify
-from flask_restful import abort
-
-
-def resp_data_invalid():
-    pass
-
-
-def resp_already_exists(msg):
-    """Response 400"""
-    response = jsonify({
-        'message': f'{msg} already exists.'
-    })
-    response.status_code = 400
-    return response
-
-
-def resp_unavailable():
-    """Response 400"""
-    response = jsonify({
-        'message': f'Room Unavailable for this Period.'
-    })
-    response.status_code = 400
-    return response
-
-
-def resp_room_linked_to_schedule(msg):
-    """Response 401"""
-    response = jsonify({
-        'message': f'This room {msg} has been reserved for one or more meetings. '
-        f'Please choose another one or try updating it. Or removed to Schedule'
-    })
-    response.status_code = 401
-    return response
-
-
-def resp_not_allowed():
-    """Response 405 Not Allowed"""
-    response = jsonify({
-        "message": "The method is not allowed for the requested URL."
-    })
-    response.status_code = 405
-    return response
-
-
-def resp_does_not_exist(resource, msg):
-    """Response 404 Not Found"""
-    if not resource:
-        abort(http_status_code=404, message=f"{msg} does not exist.")
 
 
 def resp_successful(data):
@@ -75,16 +27,16 @@ def resp_update_successfully(msg):
 
 
 def resp_delete_successfully(msg):
-    """Response 200"""
+    """Response 202"""
     response = jsonify({
         'message': f'{msg} delete successfully.'
     })
-    response.status_code = 200
+    response.status_code = 202
     return response
 
 
 def resp_not_meeting(msg):
-    """Response """
+    """Response 200"""
     messages = {
         'room': 'No meetings scheduled in this room.',
         'date': 'No meeting scheduled on this date.',
